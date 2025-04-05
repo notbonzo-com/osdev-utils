fe11bae34f5cbe20e733dd2855d8ed008be20dd0
NotBonzo <145292897+notbonzo-com@users.noreply.github.com>
2025-04-05 12:07:39 +0200

    Improved toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -1,181 +1,212 @@
 #!/bin/bash
+# Ultimate ELF Cross-Compiler Toolchain Installer
+# Non-interactive when configuration variables are preset.
+# TODO Add cmdline switches instead of relying on environment variables.
 
-###
-# NotBonzo's ultimate ELF-GCC Installer.
-###
-# @brief Build the toolchains for your OSDEV journey easily as a professional programmer.
-###
+set -e
 
-echo "--------------------------"
-echo "NotBonzo's ELF-GCC Installer"
-echo "--------------------------"
-echo ""
+ARCHITECTURE=${ARCHITECTURE:-"x86_64-elf"}
+INSTALL_DIR=${INSTALL_DIR:-"$HOME/toolchain"}
+SYMLINK=${SYMLINK:-"neither"}
+USER_SHELL=${USER_SHELL:-""}
+CUSTOM_GCC_WITH_ARCH=${CUSTOM_GCC_WITH_ARCH:-""}
+CUSTOM_GCC_WITH_TUNE=${CUSTOM_GCC_WITH_TUNE:-""}
+CUSTOM_GCC_DISABLE_SIMD=${CUSTOM_GCC_DISABLE_SIMD:-""}
+ENABLE_LANGUAGES=${ENABLE_LANGUAGES:-"c,c++"}
 
-ARCHITECTURE="x86_64-elf"
-INSTALL_DIR="$HOME/toolchain"
-SYMLINK="neither"
+declare -A ARCH_OPTIONS=(
+    [1]="x86_64-elf"
+    [2]="i686-elf"
+    [3]="i386-elf"
+    [4]="i486-elf"
+    [5]="i586-elf"
+    [6]="arm-none-eabi"
+    [7]="riscv64-elf"
+)
 
-BINUTILS_VERSION_1="2.42"
-GCC_VERSION_1="14.1.0"
-BINUTILS_VERSION_2="2.37"
-GCC_VERSION_2="11.2.0"
+declare -A VERSION_OPTIONS=(
+    [1]="2.42 14.1.0"
+    [2]="2.37 11.2.0"
+)
 
-function install_packages() {
+CACHE_DIR="/tmp/toolchain_cache"
+mkdir -p "$CACHE_DIR"
+
+function detect_distro() {
     if [ -f "/etc/os-release" ]; then
         . /etc/os-release
     else
-        echo "Error: /etc/os-release not found. Cannot determine distribution." >&2
-        exit 0
+        echo "Warning: /etc/os-release not found. Using generic settings."
+        ID="generic"
+        ID_LIKE=""
     fi
+}
+
+function install_dependencies() {
+    detect_distro
 
     declare -A pkgs=(
         [curl]="curl"
         [tar]="tar"
         [make]="make"
         [gcc]="gcc"
-        [gcc-c++]="g++"
+        [g++]="g++"
         [bison]="bison"
         [flex]="flex"
-        [gmp-devel]="libgmp-dev"
-        [libmpc-devel]="libmpc-dev"
-        [mpfr-devel]="libmpfr-dev"
+        [gmp]="libgmp-dev"
+        [mpc]="libmpc-dev"
+        [mpfr]="libmpfr-dev"
         [texinfo]="texinfo"
         [nasm]="nasm"
     )
 
-    if [[ $ID == "fedora" || $ID == "rhel" || $ID == "centos" ]]; then
-        pkgs[gcc-c++]="gcc-c++"
-        pkgs[gmp-devel]="gmp-devel"
-        pkgs[libmpc-devel]="libmpc-devel"
-        pkgs[mpfr-devel]="mpfr-devel"
-    fi
-
-    if [[ $ID == "arch" || $ID == "manjaro" ]]; then
-        pkgs[gmp-devel]="gmp"
-        pkgs[libmpc-devel]="libmpc"
-        pkgs[mpfr-devel]="mpfr"
+    if [[ "$ID" == "fedora" || "$ID" == "rhel" || "$ID" == "centos" || "$ID_LIKE" =~ "rhel" ]]; then
+        pkgs[g++]="gcc-c++"
+        pkgs[gmp]="gmp-devel"
+        pkgs[mpc]="libmpc-devel"
+        pkgs[mpfr]="mpfr-devel"
+    elif [[ "$ID" == "arch" || "$ID_LIKE" =~ "arch" ]]; then
+        pkgs[gmp]="gmp"
+        pkgs[mpc]="libmpc"
+        pkgs[mpfr]="mpfr"
     fi
 
     packages="${pkgs[@]}"
 
-    function execute_with_error_handling() {
-        if ! $@; then
-            echo "Error: Failed to execute '$@'" >&2
-            exit 0
-            # If you are wondering why I am using exit 0 everywhere, its so the quick command using `&&` deletes the script
+    run_cmd() {
+        if ! "$@"; then
+            echo "Warning: Command '$*' failed. Please check manually."
         fi
     }
 
-    case $ID in
-        debian|ubuntu|pop)
-            execute_with_error_handling sudo apt-get update
-            execute_with_error_handling sudo apt-get install -y $packages
+    case "$ID" in
+        debian|ubuntu|pop|linuxmint)
+            run_cmd sudo apt-get update
+            run_cmd sudo apt-get install -y $packages
             ;;
         fedora|rhel|centos)
-            execute_with_error_handling sudo dnf update -y
-            execute_with_error_handling sudo dnf install -y $packages
+            run_cmd sudo dnf update -y
+            run_cmd sudo dnf install -y $packages
             ;;
         arch|manjaro)
-            execute_with_error_handling sudo pacman -Syu --noconfirm $packages
+            run_cmd sudo pacman -Syu --noconfirm $packages
             ;;
-        opensuse|opensuse-leap|opensuse-tumbleweed)
-            execute_with_error_handling sudo zypper refresh
-            execute_with_error_handling sudo zypper install -y $packages
+        opensuse* )
+            run_cmd sudo zypper refresh
+            run_cmd sudo zypper install -y $packages
             ;;
         *)
-            echo "Error: Unsupported distribution '$ID'. Please install the required packages manually:" >&2
-            echo "${!pkgs[@]}"
-            exit 0
+            echo "Unsupported distro '$ID'. Please install the following packages manually:" >&2
+            echo "${pkgs[@]}"
             ;;
     esac
 }
 
-function check_requirements() {
+function validate_commands() {
     for cmd in curl tar make gcc realpath g++ bison flex nasm; do
-        if ! command -v $cmd &> /dev/null; then
-            echo "Error: Required command '$cmd' is not installed." >&2
-            read -p "Do you want to try to install it? (yes/no)" install
-            if [ "$install" = "yes" ]; then
-                install_packages
+        if ! command -v "$cmd" > /dev/null; then
+            echo "Error: '$cmd' is not installed."
+            read -p "Attempt to install dependencies? (yes/no): " response
+            if [ "$response" = "yes" ]; then
+                install_dependencies
             else
-                echo "Exiting. No changes made."
-                exit 0
+                echo "Exiting. Please install missing tools and try again."
+                exit 1
             fi
-            return
         fi
     done
 }
 
-function show_menu() {
-    echo "Please select the architecture:"
-    echo "1) x86_64-elf"
-    echo "2) i686-elf"
-    read -p "Selection (1-2): " arch_choice
-    case $arch_choice in
-        1) ARCHITECTURE="x86_64-elf";;
-        2) ARCHITECTURE="i686-elf";;
-        *) echo "Invalid selection"; exit 0;;
-    esac
+function prompt_configuration() {
+    echo "--------------------------"
+    echo "ELF Cross-Compiler Installer"
+    echo "--------------------------"
+    echo ""
 
-    read -p "Enter installation directory [$INSTALL_DIR]: " input_install_dir
-    INSTALL_DIR="${input_install_dir:-$INSTALL_DIR}"
+    if [ -z "$ARCH_FROM_ENV" ] && [ -z "${ARCH_SET}" ]; then
+        echo "Select target architecture:"
+        for key in "${!ARCH_OPTIONS[@]}"; do
+            echo "$key) ${ARCH_OPTIONS[$key]}"
+        done
+        read -p "Selection (number): " arch_choice
+        ARCHITECTURE="${ARCH_OPTIONS[$arch_choice]}"
+        if [ -z "$ARCHITECTURE" ]; then
+            echo "Invalid selection. Exiting."
+            exit 1
+        fi
+    fi
 
-    read -p "Do you want to symlink the executables to /bin, add to PATH, or neither? (symlink/path/neither) [$SYMLINK]: " action_choice
-    case $action_choice in
-        symlink|path|neither)
-            SYMLINK=$action_choice
-            ;;
-        *)
-            echo "Invalid selection. Defaulting to 'neither'."
-            SYMLINK="neither"
-            ;;
-    esac
+    if [ -z "${INSTALL_DIR_SET}" ]; then
+        read -p "Installation directory [$INSTALL_DIR]: " input_install
+        INSTALL_DIR="${input_install:-$INSTALL_DIR}"
+    fi
+
+    if [ -z "${SYMLINK_SET}" ]; then
+        read -p "Symlink executables to /usr/local/bin, add to PATH, or neither? (symlink/path/neither) [$SYMLINK]: " action_choice
+        case $action_choice in
+            symlink|path|neither)
+                SYMLINK=$action_choice
+                ;;
+            *)
+                echo "Invalid selection. Defaulting to 'neither'."
+                SYMLINK="neither"
+                ;;
+        esac
+    fi
 
     if [ "$SYMLINK" = "path" ]; then
-        DEFAULT_SHELL=$(basename "$SHELL")
-        read -p "Detected shell is $DEFAULT_SHELL. Override if different (bash/zsh/fish) or press enter to accept: " input_shell
-        USER_SHELL="${input_shell:-$DEFAULT_SHELL}"
-    fi
-    if [ "$SYMLINK" = "symlink" ]; then
-        echo ""
+        if [ -z "$USER_SHELL" ]; then
+            DEFAULT_SHELL=$(basename "$SHELL")
+            read -p "Detected shell is $DEFAULT_SHELL. Enter shell (bash/zsh/fish) or press enter to accept: " shell_input
+            USER_SHELL="${shell_input:-$DEFAULT_SHELL}"
+        fi
     fi
 
-    echo "Please select version of the toolchains:"
-    echo "1) Binutils $BINUTILS_VERSION_1 and GCC $GCC_VERSION_1"
-    echo "2) Binutils $BINUTILS_VERSION_2 and GCC $GCC_VERSION_2"
-    read -p "Selection (1-2): " version_choice
-    case $version_choice in
-        1) 
-            BINUTILS_VERSION=$BINUTILS_VERSION_1
-            GCC_VERSION=$GCC_VERSION_1
-            ;;
-        2)
-            BINUTILS_VERSION=$BINUTILS_VERSION_2
-            GCC_VERSION=$GCC_VERSION_2
-            ;;
-        *)
-            echo "Invalid selection"; exit 0;;
-    esac
+    echo "Select toolchain version:"
+    for key in "${!VERSION_OPTIONS[@]}"; do
+        read BINUTILS_VER GCC_VER <<< "${VERSION_OPTIONS[$key]}"
+        echo "$key) Binutils $BINUTILS_VER and GCC $GCC_VER"
+    done
+    read -p "Selection (number): " version_choice
+    if [ -z "${VERSION_OPTIONS[$version_choice]}" ]; then
+        echo "Invalid selection. Exiting."
+        exit 1
+    fi
+    read BINUTILS_VERSION GCC_VERSION <<< "${VERSION_OPTIONS[$version_choice]}"
 
-    echo "Configuration:"
-    echo "Architecture: $ARCHITECTURE"
-    echo "Installation Directory: $INSTALL_DIR"
-    echo "Binutils Version: $BINUTILS_VERSION"
-    echo "GCC Version: $GCC_VERSION"
+    read -p "Enter custom '--with-arch' value for GCC (or leave empty for default): " input_gcc_arch
+    CUSTOM_GCC_WITH_ARCH="${input_gcc_arch:-$CUSTOM_GCC_WITH_ARCH}"
+    read -p "Enter custom '--with-tune' value for GCC (or leave empty for default): " input_gcc_tune
+    CUSTOM_GCC_WITH_TUNE="${input_gcc_tune:-$CUSTOM_GCC_WITH_TUNE}"
+    read -p "Disable SIMD support in GCC? (yes/no) [no]: " simd_choice
+    if [ "$simd_choice" = "yes" ]; then
+        CUSTOM_GCC_DISABLE_SIMD="--disable-simd"
+    fi
+
+    read -p "Enter comma-separated list of languages to enable for GCC (--enable-languages) [${ENABLE_LANGUAGES}]: " input_languages
+    ENABLE_LANGUAGES="${input_languages:-$ENABLE_LANGUAGES}"
+
+    echo ""
+    echo "Configuration Summary:"
+    echo "  Architecture:         $ARCHITECTURE"
+    echo "  Installation Dir:     $INSTALL_DIR"
+    echo "  Binutils Version:     $BINUTILS_VERSION"
+    echo "  GCC Version:          $GCC_VERSION"
+    echo "  Enabled Languages:    $ENABLE_LANGUAGES"
+    [ -n "$CUSTOM_GCC_WITH_ARCH" ] && echo "  GCC --with-arch:      $CUSTOM_GCC_WITH_ARCH"
+    [ -n "$CUSTOM_GCC_WITH_TUNE" ] && echo "  GCC --with-tune:      $CUSTOM_GCC_WITH_TUNE"
+    [ -n "$CUSTOM_GCC_DISABLE_SIMD" ] && echo "  GCC SIMD support:     Disabled"
     if [ "$SYMLINK" = "path" ]; then
-        echo "Adding folder to path: Yes"
-        echo "Shell: $USER_SHELL"
-        echo "Symlink executables to /bin: No"
-    fi
-    if [ "$SYMLINK" = "symlink" ]; then
-        echo "Adding folder to path: No"
-        echo "Shell: irrelevant"
-        echo "Symlink executables to /bin: Yes"
+        echo "  PATH update:          Yes (Shell: $USER_SHELL)"
+    elif [ "$SYMLINK" = "symlink" ]; then
+        echo "  Symlink executables:  Yes (to /usr/local/bin)"
+    else
+        echo "  PATH/symlink:         No changes"
     fi
 }
 
-function confirm_and_proceed() {
-    read -p "Are you sure you want to proceed with these settings? (yes/no): " confirm
+function confirm_configuration() {
+    read -p "Proceed with these settings? (yes/no): " confirm
     if [ "$confirm" != "yes" ]; then
         echo "Installation aborted."
         exit 0
@@ -184,10 +215,10 @@ function confirm_and_proceed() {
 
 function check_existing_installation() {
     if [ -d "$INSTALL_DIR/$ARCHITECTURE" ]; then
-        echo "Warning: Toolchain already installed in $INSTALL_DIR/$ARCHITECTURE"
-        read -p "Do you want to overwrite? (yes/no): " overwrite
+        echo "Warning: Toolchain already exists at $INSTALL_DIR/$ARCHITECTURE."
+        read -p "Overwrite existing installation? (yes/no): " overwrite
         if [ "$overwrite" != "yes" ]; then
-            echo "Exiting. No changes made."
+            echo "Exiting without changes."
             exit 0
         fi
         rm -rf "$INSTALL_DIR/$ARCHITECTURE"
@@ -195,103 +226,90 @@ function check_existing_installation() {
 }
 
 function add_to_path() {
-    local toolchain_path="$TOOLCHAIN_PREFIX/bin"
-    case $USER_SHELL in
+    local toolchain_bin="$TOOLCHAIN_PREFIX/bin"
+    case "$USER_SHELL" in
         bash)
-            echo "export PATH=\$PATH:$toolchain_path" >> ~/.bashrc
+            echo "export PATH=\$PATH:$toolchain_bin" >> ~/.bashrc
             ;;
         zsh)
-            echo "export PATH=\$PATH:$toolchain_path" >> ~/.zshrc
+            echo "export PATH=\$PATH:$toolchain_bin" >> ~/.zshrc
             ;;
         fish)
-            echo "set -U fish_user_paths $toolchain_path \$fish_user_paths" >> ~/.config/fish/config.fish
+            echo "set -U fish_user_paths $toolchain_bin \$fish_user_paths" >> ~/.config/fish/config.fish
             ;;
         *)
-            echo "Unsupported shell. Please add $toolchain_path to your PATH manually."
+            echo "Unsupported shell. Please add $toolchain_bin to your PATH manually."
             ;;
     esac
-    echo "Please restart your terminal or source your shell configuration file to apply PATH changes."
+    echo "Reload your shell configuration to update PATH."
 }
 
-function install_toolchain() {
-    echo "Installing toolchain for $ARCHITECTURE in $INSTALL_DIR..."
+function fetch_file() {
+    local url="$1"
+    local filename="${url##*/}"
+    local cache_file="$CACHE_DIR/$filename"
+    if [ -f "$cache_file" ] && [ -s "$cache_file" ]; then
+        echo "Using cached $filename from $CACHE_DIR"
+    else
+        echo "Downloading $filename..."
+        curl -Lo "$cache_file" "$url" || { echo "Error: Failed to download $filename"; exit 1; }
+    fi
+    [ ! -f "$filename" ] && cp "$cache_file" .
+}
+
+function build_toolchain() {
+    echo "Building toolchain for $ARCHITECTURE in $INSTALL_DIR..."
     TOOLCHAIN_PREFIX=$(realpath -m "$INSTALL_DIR/$ARCHITECTURE")
-
     mkdir -p "$TOOLCHAIN_PREFIX"
-
-    cd "$TOOLCHAIN_PREFIX" || { echo "Failed to change directory to $TOOLCHAIN_PREFIX"; exit 0; }
-
+    cd "$TOOLCHAIN_PREFIX" || { echo "Cannot access directory $TOOLCHAIN_PREFIX"; exit 1; }
     MAKEFLAGS="-j$(nproc)"
 
-    function fetch() {
-        url="$1"
-        filename="${url##*/}"
-        if [ ! -f "$filename" ]; then
-            echo "Downloading $filename..."
-            curl -LO "$url" || { echo "Failed to download $filename"; exit 0; }
-        fi
-        if [ ! -s "$filename" ]; then
-            echo "Error: Downloaded file $filename is empty." >&2
-            exit 0
-        fi
-    }
+    local binutils_url="https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz"
+    local gcc_url="https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz"
 
-    BINUTILS_URL="https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz"
-    GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz"
+    fetch_file "$binutils_url"
+    fetch_file "$gcc_url"
 
-    fetch "$GCC_URL"
-    fetch "$BINUTILS_URL"
+    echo "Extracting sources..."
+    tar -xf "binutils-${BINUTILS_VERSION}.tar.xz" || { echo "Extraction failed for Binutils."; exit 1; }
+    tar -xf "gcc-${GCC_VERSION}.tar.xz" || { echo "Extraction failed for GCC."; exit 1; }
 
-    echo "Extracting GCC..."
-    tar -xf "gcc-${GCC_VERSION}.tar.xz" || { echo "Failed to extract GCC."; exit 0; }
-    echo "Extracting Binutils..."
-    tar -xf "binutils-${BINUTILS_VERSION}.tar.xz" || { echo "Failed to extract Binutils."; exit 0; }
+    mkdir -p build-binutils build-gcc
 
-    mkdir -p "$TOOLCHAIN_PREFIX/build-binutils"
-    mkdir -p "$TOOLCHAIN_PREFIX/build-gcc"
-
-    pushd "$TOOLCHAIN_PREFIX/build-binutils" > /dev/null
+    pushd build-binutils > /dev/null
     ../binutils-${BINUTILS_VERSION}/configure \
-        --prefix="$TOOLCHAIN_PREFIX"              \
-        --target=$ARCHITECTURE                    \
-        --enable-targets=x86_64-pe                \
-        --with-sysroot                            \
-        --disable-nls                             \
+        --prefix="$TOOLCHAIN_PREFIX" \
+        --target="$ARCHITECTURE" \
+        --with-sysroot \
+        --disable-nls \
         --disable-werror && make $MAKEFLAGS && make install
     popd > /dev/null
 
-    pushd "$TOOLCHAIN_PREFIX/build-gcc" > /dev/null
-    ../gcc-${GCC_VERSION}/configure \
-        --prefix="$TOOLCHAIN_PREFIX"  \
-        --target=$ARCHITECTURE        \
-        --disable-nls                 \
-        --enable-languages=c,c++      \
-        --without-headers && make $MAKEFLAGS all-gcc all-target-libgcc && make install-gcc install-target-libgcc
+    pushd build-gcc > /dev/null
+    GCC_CONFIG_OPTS="--prefix=$TOOLCHAIN_PREFIX --target=$ARCHITECTURE --disable-nls --enable-languages=${ENABLE_LANGUAGES} --without-headers"
+    [ -n "$CUSTOM_GCC_WITH_ARCH" ] && GCC_CONFIG_OPTS+=" --with-arch=$CUSTOM_GCC_WITH_ARCH"
+    [ -n "$CUSTOM_GCC_WITH_TUNE" ] && GCC_CONFIG_OPTS+=" --with-tune=$CUSTOM_GCC_WITH_TUNE"
+    [ -n "$CUSTOM_GCC_DISABLE_SIMD" ] && GCC_CONFIG_OPTS+=" $CUSTOM_GCC_DISABLE_SIMD"
+    ../gcc-${GCC_VERSION}/configure $GCC_CONFIG_OPTS && make $MAKEFLAGS all-gcc all-target-libgcc && make install-gcc install-target-libgcc
     popd > /dev/null
 
     if [ "$SYMLINK" = "symlink" ]; then
-        echo "Symlinking executables to /bin..."
-        declare -a tools=("gcc" "ld" "as" "g++")
-        for tool in "${tools[@]}"; do
-            src="$TOOLCHAIN_PREFIX/bin/$ARCHITECTURE-$tool"
-            dest="/usr/local/bin/$ARCHITECTURE-$tool"
+        echo "Creating symlinks in /usr/local/bin..."
+        for tool in gcc ld as g++; do
+            local src="$TOOLCHAIN_PREFIX/bin/$ARCHITECTURE-$tool"
+            local dest="/usr/local/bin/$ARCHITECTURE-$tool"
             [ -L "$dest" ] && sudo rm "$dest"
-            sudo ln -s "$src" "$dest" || { echo "Failed to create symlink for $tool"; exit 0; }
+            sudo ln -s "$src" "$dest" || { echo "Symlink failed for $tool."; exit 1; }
         done
     elif [ "$SYMLINK" = "path" ]; then
         add_to_path
-        echo "Added $TOOLCHAIN_PREFIX/bin to your $USER_SHELL PATH."
     fi
 
-    echo "Installation complete."
-    echo "Run $ARCHITECTURE-gcc for the GNU C Compiler"
-    echo "Run $ARCHITECTURE-ld for the GNU Linker"
-    echo "Run $ARCHITECTURE-as for the GNU Assembler"
-    echo "Run $ARCHITECTURE-g++ for the GNU C++ Compiler"
+    echo "Installation complete. Use ${ARCHITECTURE}-gcc, ${ARCHITECTURE}-ld, ${ARCHITECTURE}-as, and ${ARCHITECTURE}-g++ to invoke the toolchain."
 }
 
-check_requirements
-show_menu
-confirm_and_proceed
+validate_commands
+prompt_configuration
+confirm_configuration
 check_existing_installation
-install_toolchain
+build_toolchain
